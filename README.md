884a386b2be5063dfddc3dd314312ddfd2ab0804
Leon Sander <leon.sander@protonmail.com>
2021-09-30 13:52:23 +0200

    small update

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-#### Image sequence to Video converter - mot15
+#### Image sequence to Video converter - MOT
 
-    Change the train and test paths in the mot15_conversion.py file.
+    Change the train and test paths in the mot_conversion.py file.
     The data has to be in the folder which got extracted after downloading all data from the mot site.
     https://motchallenge.net/data/MOT15/
 
-    Then run mot15_conversion.py
+    Then run mot_conversion.py
     The videos will be stored in the folders which contain the images
+
+    The code works with mot data from all years
--- mot_conversion.py
+++ mot_conversion.py
@@ -1,22 +1,24 @@
 import configparser
 from converter import converter
 import glob
+from tqdm import tqdm
 
 config_parser = configparser.ConfigParser()
 conv = converter()
 
-path_train = "" #MOT15/train/
-path_test = "" #MOT15/test/
+path_train = '' #MOTXX/train/
+path_test =  '' #MOTXX/test/
 test_train_paths = [path_train, path_test]
 
-for path_t in test_train_paths:
+for path_t in tqdm(test_train_paths):
     paths = glob.glob(path_t + "*")
     dirs_and_names = []
     for path in paths:
         name = path.replace(path_t, '')
         dirs_and_names.append((path, name))
 
-    for folder in dirs_and_names:
+
+    for folder in tqdm(dirs_and_names):
         config_path = folder[0] + '/seqinfo.ini'
         name = folder[1]
         imgs_path = folder[0]  + '/img1/'
